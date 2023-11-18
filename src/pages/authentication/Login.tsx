099a46b63229010d052b66c4a0a4a28a34feac80
Marek Gerszendorf <>
2023-11-18 15:07:26 +0100

    feat: add login and register layout

--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -1,9 +1,26 @@
+import { TextField, Button, Paper, Box, Typography } from '@mui/material';
+
 const Login = () => {
     return (
-        <>
-            <h2>Login</h2>
-        </>
-    )
-}
+        <Box display="flex" alignItems="center" justifyContent="center" width="100%">
+            <Paper elevation={3} className='login-form-container'>
+                <Typography variant="h4" component="h2" gutterBottom>
+                    Login
+                </Typography>
+                <form>
+                    <Box marginBottom={2}>
+                        <TextField fullWidth label="Username" variant="outlined" />
+                    </Box>
+                    <Box marginBottom={2}>
+                        <TextField fullWidth label="Password" type="password" variant="outlined" />
+                    </Box>
+                    <Button variant="contained" color="primary" fullWidth>
+                        Log In
+                    </Button>
+                </form>
+            </Paper>
+        </Box>
+    );
+};
 
-export default Login
+export default Login;
--- src/pages/authentication/Register.tsx
+++ src/pages/authentication/Register.tsx
@@ -1,9 +1,32 @@
+import { TextField, Button, Paper, Box, Typography } from '@mui/material';
+
 const Register = () => {
     return (
-        <>
-            <h2>Register</h2>
-        </>
-    )
-}
+        <Box display="flex" alignItems="center" justifyContent="center" width="100%">
+            <Paper elevation={3} className='register-form-container'>
+                <Typography variant="h4" component="h2" gutterBottom>
+                    Register
+                </Typography>
+                <form>
+                    <Box marginBottom={2}>
+                        <TextField fullWidth label="Username" variant="outlined" />
+                    </Box>
+                    <Box marginBottom={2}>
+                        <TextField fullWidth label="Email" type="email" variant="outlined" />
+                    </Box>
+                    <Box marginBottom={2}>
+                        <TextField fullWidth label="Password" type="password" variant="outlined" />
+                    </Box>
+                    <Box marginBottom={2}>
+                        <TextField fullWidth label="Confirm Password" type="password" variant="outlined" />
+                    </Box>
+                    <Button variant="contained" color="primary" fullWidth>
+                        Register
+                    </Button>
+                </form>
+            </Paper>
+        </Box>
+    );
+};
 
-export default Register
+export default Register;
